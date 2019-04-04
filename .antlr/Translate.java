04abe72fa593743b8adc157d47884a4e09fcd8f7
Galiold <ali.goldani@ymail.com>
2019-04-04 18:48:31 +0430

    Java files uploaded

---
+++ .antlr/Translate.java
@@ -0,0 +1,27 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Translate
+{
+    public static void main(String[] args) throws Exception
+    {
+        // CharStream that reads from standard input
+        CharStream stream = CharStreams.fromFileName("../testfiles/test");
+        // Lexer that feeds off of input CharStream
+        JythonLexer lexer = new JythonLexer(stream); 
+        // ‌Buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer); 
+        // Parser that feeds off the tokens buffer
+        JythonParser parser = new JythonParser(tokens);
+        // begin parsing at prog rule - the start rule
+        ParseTree tree = parser.prog();
+        // A generic parse tree walker that can trigger callbacks
+        ParseTreeWalker walker = new ParseTreeWalker();
+        // Walk the tree created during the parse, trigger callbacks
+        walker.walk(new MyListener(), tree);
+        // print a \n after translation
+        System.out.println();
+    }
+
+     
+}
